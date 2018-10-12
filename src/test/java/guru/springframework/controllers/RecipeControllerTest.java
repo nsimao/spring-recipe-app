b30cf2269ba44cc29f8581b2624f014183ffc3d0
nelsons <nelson.simao@me.com.br>
2018-10-12 13:43:32 -0300

    tests

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -10,7 +10,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -29,8 +28,7 @@ public class RecipeControllerTest {
 
     @Mock
     RecipeService recipeService;
-    @Mock
-    Model model;
+
     RecipeController controller;
 
     MockMvc mockMvc;
@@ -40,6 +38,7 @@ public class RecipeControllerTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+
         controller = new RecipeController(recipeService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
@@ -49,12 +48,14 @@ public class RecipeControllerTest {
         mockMvc.perform(get("/recipe/1/delete"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/"));
+
         verify(recipeService, times(1)).deleteById(anyLong());
     }
 
     @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
+
         mockMvc.perform(get("/recipe/new"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/recipeform"))
@@ -67,6 +68,7 @@ public class RecipeControllerTest {
         recipe.setId(1L);
 
         when(recipeService.findById(anyLong())).thenReturn(recipe);
+
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/show"))
@@ -77,7 +79,9 @@ public class RecipeControllerTest {
     public void testGetUpdateView() throws Exception {
         RecipeCommand command = new RecipeCommand();
         command.setId(2L);
+
         when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
         mockMvc.perform(get("/recipe/1/update"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/recipeform"))
@@ -88,11 +92,14 @@ public class RecipeControllerTest {
     public void testPostNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
         command.setId(2L);
+
         when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
-                .param("description", "some string"))
+                .param("description", "some string")
+        )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.converters.RecipeCommandToRecipe;
 import guru.springframework.converters.RecipeToRecipeCommand;
@@ -22,15 +23,17 @@ import static org.mockito.Mockito.*;
  * @author Nelson Simão
  * @since 1.0
  */
-
 public class RecipeServiceImplTest {
 // ------------------------------ FIELDS ------------------------------
 
     RecipeServiceImpl recipeService;
+
     @Mock
     RecipeRepository recipeRepository;
+
     @Mock
     RecipeToRecipeCommand recipeToRecipeCommand;
+
     @Mock
     RecipeCommandToRecipe recipeCommandToRecipe;
 
@@ -56,11 +59,16 @@ public class RecipeServiceImplTest {
         Recipe recipe = new Recipe();
         recipe.setId(1L);
         Optional<Recipe> recipeOptional = Optional.of(recipe);
+
         when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
         RecipeCommand recipeCommand = new RecipeCommand();
         recipeCommand.setId(1L);
+
         when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
         RecipeCommand commandById = recipeService.findCommandById(1L);
+
         assertNotNull("Null recipe returned", commandById);
         verify(recipeRepository, times(1)).findById(anyLong());
         verify(recipeRepository, never()).findAll();
@@ -69,10 +77,10 @@ public class RecipeServiceImplTest {
     @Test
     public void getRecipesTest() throws Exception {
         Recipe recipe = new Recipe();
-        HashSet recipesData = new HashSet();
-        recipesData.add(recipe);
+        HashSet receipesData = new HashSet();
+        receipesData.add(recipe);
 
-        when(recipeRepository.findAll()).thenReturn(recipesData);
+        when(recipeService.getRecipes()).thenReturn(receipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
@@ -84,6 +92,7 @@ public class RecipeServiceImplTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+
         recipeService = new RecipeServiceImpl(recipeRepository, recipeCommandToRecipe, recipeToRecipeCommand);
     }
 
@@ -91,9 +100,12 @@ public class RecipeServiceImplTest {
     public void testDeleteById() throws Exception {
         //given
         Long idToDelete = Long.valueOf(2L);
+
         //when
         recipeService.deleteById(idToDelete);
+
         //no 'when', since method has void return type
+
         //then
         verify(recipeRepository, times(1)).deleteById(anyLong());
     }
